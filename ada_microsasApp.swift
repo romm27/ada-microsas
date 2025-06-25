6690b5a9fce4fa038ccbf7513ca1d6d7e09d0e4a
Giovanni Strasser <53452941+romm27@users.noreply.github.com>
2025-06-25 15:43:35 -0300

    Initial commit

---
+++ ada_microsasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ada_microsasApp.swift
+//  ada-microsas
+//
+//  Created by Giovanni Galarda Strasser on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ada_microsasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
